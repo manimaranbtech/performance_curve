031dd75de6b45dae17201f22251e6846229bf9cc
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-12 13:41:49 +0530

    Update perf.py

--- perf.py
+++ perf.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
+ # Load data from CSV file
+    data = pd.read_csv('performance.csv')
+    
+    # Extract stretch ratio and true stress from CSV
+    current = data['current'].values
+    torque = data['torque'].values
+    speed = data['speed'].values
