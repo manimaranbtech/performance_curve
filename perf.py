0d4735692c8dd3b9b107a23082775e2201bff0b5
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-12 17:44:56 +0530

    Update perf.py

--- perf.py
+++ perf.py
@@ -16,14 +16,14 @@ speed = data['speed'].values
 y2 = int(input("Enter battery voltage: "))
 x2 = int(input("Enter battery current: "))
 x1 = 0
-y1 = 0
+y1 = 12
 m = (y2-y1)/(x2-x1)
 A_last = current[-1]+10    #last value from current array
 current_bat_line = np.linspace(0, A_last, 10)
 print(A_last)
 print(current_bat_line)
 print(m)
-voltage = -(current_bat_line*m) + 12
+voltage = (current_bat_line*m) + 12
 print(voltage)
 plt.plot(current, voltage)
 plt.savefig('my_plot.png')
