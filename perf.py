b38226401d0c2577167ed8d7c5c3907d09d1c8ad
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-03-12 13:43:54 +0530

    Update perf.py

--- perf.py
+++ perf.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
  # Load data from CSV file
-    data = pd.read_csv('performance.csv')
+    file_csv = input("Enter file name: ")
+    data = pd.read_csv('file_csv')
     
     # Extract stretch ratio and true stress from CSV
     current = data['current'].values
